ae48c45251a5c82ea7b478982af8b585f6a60eaa
Omiefa <bristolmelvin@yahoo.com>
2023-10-06 01:49:45 +0100

    task1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} x {} = {}".format(a, b, mul(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
 
 
