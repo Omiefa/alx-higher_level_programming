cf01cc2166a0ed418ee146c43b0539c109d83b65
Omiefa <bristolmelvin@yahoo.com>
2023-10-06 01:17:22 +0100

    task0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    "print the sum of 1 and 2"
+
+    import add_0
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
