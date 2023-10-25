70d7a3c7e5ad327871598af174eed512d2d0ba23
Omiefa <bristolmelvin@yahoo.com>
2023-10-25 03:04:10 +0100

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# 0-square.py by Melvin
+"""Defines a square"""
+
+
+class Square:
+    """Represnts a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square clas
+        Args:
+               size - represents the size of the defined square
+        Raises:
+               TypeError: if size is not integer
+               ValueError: if size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """method that retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """method that prints in stdout the square with the #"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
