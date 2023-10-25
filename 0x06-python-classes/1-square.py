d026b7d929c5ef23f5f2c70a25c5027d096ca2ff
Omiefa <bristolmelvin@yahoo.com>
2023-10-25 01:45:58 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-square.py by Melvin
+"""Defines a square"""
+
+
+class Square:
+    """Represnts a square"""
+
+    def __init__(self, size):
+        """Initializing this square clas
+	Args: size - represents the size of the defined square"""
+
+        self.__size = size
