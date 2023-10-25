d04db38d011cf0b98d7ae1f6811d6a9ae7dd4636
Omiefa <bristolmelvin@yahoo.com>
2023-10-25 02:35:57 +0100

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 0-square.py by Melvin
+"""Defines a square"""
+
+
+class Square:
+    """Represnts a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square clas
+        Args:
+               size - represents the size of the defined square
+        Raises:
+               TypeError: if size is not integer
+               ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
