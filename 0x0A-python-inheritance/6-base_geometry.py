21503f9669dbe1879bdb186d3aa4b37b9eff6ff7
Omiefa <bristolmelvin@yahoo.com>
2023-11-07 02:11:59 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A module that defines a base geometry class"""
+
+
+class BaseGeometry:
+    """This class represnts geometry"""
+    def area(self):
+        """Raise an Exception"""
+        raise Exception("area() is not implemented")
