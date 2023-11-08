04b1f34220456a962102c9eb81b3be2e1215703d
Omiefa <bristolmelvin@yahoo.com>
2023-11-08 01:17:28 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module that read a text and print to stdout"""
+
+
+def read_file(filename=""):
+    """A function that reads and prints a text
+       file to the stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
+
+
